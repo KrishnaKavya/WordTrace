2695893ee9ccb382fcf1e2e5857fb755d032e73d
KrishnaKavya <m.krishnakavya@gmail.com>
2016-08-03 00:07:21 -0500

    First checkin

---
+++ WordTrace.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Word Trace</title>
+
+	<!--Jquery Library-->
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!--CSS Style Sheet-->
+	<link rel="stylesheet" type="text/css" href="style/WordTraceStyle.css">
+</head>
+
+<body>
+<div class="container-fluid">
+<!--Title-->  
+<header>
+<h1 class="title">Word Trace</h1>
+</header>
+<!--Instructions Row 1-->
+<div class="row">
+
+<p class="Instructions">
+<b>Instructions:</b>  Identify words you find confusing or interesting.  Select one of the words and left click on it.  Read the dictionary entries.  Fill out the form.  (See &quot; WordTrace&quot; in &quot;Resources&quot; for additional information on how to do this.)  Return to the poem and hover over the word to see the &quot; WordTrace &quot; you have created.  Follow the same procedure with other words.  In addition to difficult words, choose some familiar words to explore more deeply.  Document your work in your log and save screenshots of your results.</p>
+</div>
+<!--Row 2-->
+<div class="row">
+<!--First Column-->
+<div class="col-sm-6 col-md-6 col-lg-6">
+<p id="poem">
+
+<span>Black</span> A, <span>white</span> E, <span>red</span> I, <span>green</span> U, <span>blue</span> O - <span>vowels</span>,<br>		
+<span>Some</span> <span>day</span> I will <span>open</span> your <span>silent</span> <span>pregnancies</span>:<br>
+A, <span>black</span> <span>belt</span>, <span>hairy</span> with <span>burst</span> <span>flies</span>,<br>
+Bumbling and buzzing over stinking cruelties,<br>
+<br>
+Pits of night; E, candour of sand pavilions,<br>
+High glacial spears, white kings, trembling Queen Anne's lace;<br>
+I, bloody spittle, laughter dribbling from a face<br>
+In wild denial or in anger, vermilions;<br>
+<br>
+U,&#46;&#46;&#46; divine movement of viridian seas,<br>
+Peace of pastures animal-strewn, peace of calm lines<br>
+Drawn on foreheads worn with heavy alchemies;<br>
+<br>
+O, supreme Trumpet, harsh with strange stridencies,<br>
+Silences traced in angels and astral designs:<br>
+O&#46;&#46;&#46;Omega&#46;&#46;&#46;the violet light of His Eyes!<br>
+
+</p>
+<!--End of first Column-->
+</div>
+
+<!--Second column-->
+<div class="col-sm-6 col-md-6 col-lg-6" id="Phase Box">
+<!---->	
+<div class="row">
+</div>
+<div class="row">
+</div>
+	
+
+<!--Second column end-->
+</div>
+<!--Second row end-->
+</div>
+<!--Container End-->
+</div>
+
+
+</body>
+</html>
