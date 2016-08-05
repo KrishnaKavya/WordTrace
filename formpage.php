4082c857c556099305fb1783e7c09dbe0ec8a39a
KrishnaKavya <m.krishnakavya@gmail.com>
2016-08-04 21:15:12 -0500

    form page structure

---
+++ formpage.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form</title>
+
+	<!--Jquery Library-->
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!--CSS Style Sheet-->
+	<link rel="stylesheet" type="text/css" href="style/WordTraceStyle.css">
+
+</head>
+<body>
+<!--Main Div-->
+<div class="container-fluid">
+<!--Free Dictionary link-->
+<div class="row">
+	
+</div>
+
+<!--Form-->
+<div class="row">
+<!--Change to a, b,c and add an inner bullet for Origins-->
+<ol>
+<li>
+Definitions
+</li>
+<li>
+Associations
+</li>	
+<li>
+Connections
+</li>
+</ol>
+</div>
+</div>
+
+</body>
+</html>
