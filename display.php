c02185f74daf6ccd02ccb9076d50377f738782f3
KrishnaKavya <m.krishnakavya@gmail.com>
2016-08-25 15:39:08 -0500

    layout 2.0

--- display.php
+++ display.php
@@ -3,7 +3,16 @@
 <head>
 	<title></title>
 </head>
+
+<style type="text/css">
+div{
+
+}
+</style>
 <body>
+<div class="col-xs-4 ol-sm-4 col-md-4 col-lg-4">
+<br><br><br><br>
 <h1>Click on words to continue</h1>
+</div>
 </body>
 </html>
