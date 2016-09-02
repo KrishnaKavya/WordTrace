c6d6e1e6a4709abaa671612a4e232b495974fcbe
KrishnaKavya <m.krishnakavya@gmail.com>
2016-09-02 12:37:37 -0500

    read me

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # WordTrace
+_________________________________________________________________________________________________________
+ 
+Word trace is an activity developed as a part of research experiment on the poem vowels written by Arthur Rimbaud(1854-1891).
+
+Idea of the project: 
+	Every person has different experiences in life and interprets the poem in their own way. 
+	Lets consider the word "RED", Red is te word the resembles love and passion. But at the same time it may be a symbol of sacrifice of a soldier in the war.
+		
+	The main idea about the project is to understand the interpretations of every individual and the way they can connect to the poem. 
+
+
+
+#References
+__________________________________________________________________________________________________________
+
+http://www.doctorhugo.org/synaesthesia/rimbaud.html
--- WordTrace.php
+++ WordTrace.php
@@ -3,23 +3,25 @@ require_once('mysqli_connect.php'); 	//Including the database connection and est
 session_start();						//Session start.
 $tablename="wordtrace";		// The table name is fetched from the session variables 
 //word should be changed.
-$word=$_REQUEST['word'];
-$username=$_SESSION['user_login'];
-
+$word=$_REQUEST['word'];	// The word is fetched from the url. 
+$username=$_SESSION['user_login'];	// The user name of the user is fetched from the session variables
+// The select query fetches the records with the word and the username. 
 $selectQuery="SELECT * from $tablename where word='$word' and username='$username'";
+//execution of the query. 
 $result=@mysqli_query($dbc, $selectQuery);
+//When the record exists. 
 if ($result->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
-    	
+    while($row = $result->fetch_assoc()) {	//fetching independent values into row variable.
+    	/*
+		  The explode function is used to split the array of words seperated with comma. 
+		  The return type of explode function is array. The array is stored is variables. 
+    	*/
     	$origin=explode(",",$row["origin"]);
     	$past=explode(",",$row["past"]);
     	$present=explode(",",$row["present"]);
     	$associations=explode(",",$row["associations"]);
     	$connections=explode(",",$row["connections"]);
-
-
-//    	echo $origin.$past.$present.$associations.$connections;
     }
 }
 
@@ -51,14 +53,35 @@ if ($result->num_rows > 0) {
 
 <!--Main container-->
 <div class="container-fluid">
-
+<!--The word trace is displayed in the first 7 coulmn grid of the page.-->
 <div  class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
+<!--
+The container in inturn divided into a 3*3 matrix.
+- 3 rows and 3 coulmns. 
+ The content is spread randomly on the columns and rows. 
+- The page is designed on a div design. 
+  _________________________________
+ |			|        | 			   |
+ |	 origin	|		 | connections |	
+ |__________|________|_____________|
+ | 			|		 |			   |
+ |  value 	|  word  |             |		
+ |__________|________|_____________|
+ |			|		 |			   |
+ | Present  |		 | associations|
+ |__________|________|_____________|
+-->
+
 <!--Row One-->
 <div class="row">
 
 <div  class="col-xs-7 ol-sm-7 col-md-6 col-lg-7">
 <h2 class="origin">
+<!--Origin segment--> 
 <?php 
+	/*
+	 for each loop to access all the elements in the origin array. 
+	*/
 	foreach ($origin as $value) {
 		echo $value;
 		?> <br>
@@ -67,10 +90,13 @@ if ($result->num_rows > 0) {
 ?>
 </h2>
 </div>
-
+<!--connection segment-->
 <div  class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
 <h2 class="connections">
 <?php 
+	/*
+	 for each loop to access all the elements in the origin array. 
+	*/
 	foreach ($connections as $value) {
 		echo $value; 
 		?> <br>
@@ -85,8 +111,12 @@ if ($result->num_rows > 0) {
 <!--Row two-->
 <div class="row">
 <div  class="col-xs-3 col-sm-4 col-md-4 col-lg-4">
+<!--past segment-->
 <h2 class="past">
 <?php 
+	/*
+	 for each loop to access all the elements in the origin array. 
+	*/
 	foreach ($past as $value) {
 		echo $value;
 		?> <br>
@@ -97,6 +127,7 @@ if ($result->num_rows > 0) {
 </div>
 
 <div  class="col-xs-3 col-sm-4 col-md-4 col-lg-4">
+<!--Word-->
 <h1 class="Word">
 <?php echo $word?>
 </h1>
@@ -109,8 +140,12 @@ if ($result->num_rows > 0) {
 <!--Row 3-->
 <div class="row">
 <div  class="col-xs-3 col-sm-4 col-md-4 col-lg-4">
+<!--present segment-->
 <h2 class="present">
 <?php
+/*
+	 for each loop to access all the elements in the origin array. 
+	*/
 foreach ($present as $value) {
 		echo $value;
 		?> <br>
@@ -124,9 +159,12 @@ foreach ($present as $value) {
 </div>
 
 <div  class="col-xs-3 col-sm-4 col-md-4 col-lg-4">
+<!--Association segment-->
 <h2 class="associations">
 <?php 
-
+/*
+	 for each loop to access all the elements in the origin array. 
+	*/
 foreach ($associations as $value) {
 		echo $value;
 		?> <br>
